25e27f593fb3baab9f875188facd667027f8a56f
Korey Hermann <khermann135@gmail.com>
2023-05-23 21:57:30 -0400

    fixed page switching

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-function Navbar() {
-  function adoption(){
-    console.log(document.querySelector("#adoption"))
-    document.querySelector("#adoption").style="display: block"
-    document.querySelector("#PetServices").style="display: none"
-    document.querySelector("#PetProfile").style="display: none"
-  }
-  function PetServices(){
-    console.log(document.querySelector("#adoption"))
-    document.querySelector("#adoption").style="display: none"
-    document.querySelector("#PetServices").style="display: block"
-    document.querySelector("#PetProfile").style="display: none"
-  }
-  function PetProfile(){
-    console.log(document.querySelector("#adoption"))
-    document.querySelector("#adoption").style="display: none"
-    document.querySelector("#PetServices").style="display: none"
-    document.querySelector("#PetProfile").style="display: block"
-  }
- 
+import React from 'react';
+
+
+function Navbar({ currentTab, handleTabChange }) {
+
   return (
+
+
     <nav>
-      <ul>
-        <li><button onClick={adoption}>Adoption</button></li>
-        <li><button onClick={PetServices}>PetServices</button></li>
-        <li><button onClick={PetProfile}>PetProfile</button></li>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#SignupForm"
+            onClick={() => handleTabChange('SignupForm')}
+            className={currentTab === 'SignupForm' ? 'nav-link active' : 'nav-link'}
+          >
+            Sign Up
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Adoption"
+            onClick={() => handleTabChange('Adoption')}
+            className={currentTab === 'Adoption' ? 'nav-link active' : 'nav-link'}
+          >
+            Adoption
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#PetServices"
+            onClick={() => handleTabChange('PetServices')}
+            className={currentTab === 'PetServices' ? 'nav-link active' : 'nav-link'}
+          >
+            Pet Services
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#PetProfile"
+            onClick={() => handleTabChange('PetProfile')}
+            className={currentTab === 'PetProfile' ? 'nav-link active' : 'nav-link'}
+          >
+            Pet Profile
+          </a>
+        </li>
       </ul>
     </nav>
+
   );
 }
+
+
+
+
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function Navbar() {
+//   function adoption(){
+//     console.log(document.querySelector("#adoption"))
+//     document.querySelector("#adoption").style="display: block"
+//     document.querySelector("#PetServices").style="display: none"
+//     document.querySelector("#PetProfile").style="display: none"
+//   }
+//   function PetServices(){
+//     console.log(document.querySelector("#adoption"))
+//     document.querySelector("#adoption").style="display: none"
+//     document.querySelector("#PetServices").style="display: block"
+//     document.querySelector("#PetProfile").style="display: none"
+//   }
+//   function PetProfile(){
+//     console.log(document.querySelector("#adoption"))
+//     document.querySelector("#adoption").style="display: none"
+//     document.querySelector("#PetServices").style="display: none"
+//     document.querySelector("#PetProfile").style="display: block"
+//   }
+
+//   return (
+//     <nav>
+//       <ul>
+//         <li><button onClick={adoption}>Adoption</button></li>
+//         <li><button onClick={PetServices}>PetServices</button></li>
+//         <li><button onClick={PetProfile}>PetProfile</button></li>
+//       </ul>
+//     </nav>
+//   );
+// }
 
 
 // function Navbar() {
