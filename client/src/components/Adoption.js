b31844937ede6234776ef47ad30e9677ab4be38e
jshimetz <jeremyshimetz@gmail.com>
2023-05-23 20:38:28 -0500

    Deleting Google Maps data

--- client/src/components/Adoption.js
+++ client/src/components/Adoption.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 function Adoption() {
   return (
