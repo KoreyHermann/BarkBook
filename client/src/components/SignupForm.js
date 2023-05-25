4b6b3b2d4cdd979f01f5ebf20e544f1fd66fba24
Martin Cuellar <martinhc129@gmail.com>
2023-05-25 16:31:52 -0400

    fixed signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,108 +1,70 @@
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import '../assets/Signup.css';
 
-import { createUser } from '../utils/createUser';
-import Auth from '../utils/auth';
+const SignupForm = ({ isLoggedIn, setLoggedIn, setUsername, handleLogout }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameInput, setUsernameInput] = useState('');
 
-const SignupForm = () => {
-  // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
-  const [validated] = useState(false);
-  // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
+  const handleSignupLogin = (e) => {
+    e.preventDefault();
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    if (email && password && usernameInput) {
+      setLoggedIn(true);
+      setUsername(usernameInput);
+    }
   };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+  const handleSignOut = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    handleLogout();
   };
+
+  if (isLoggedIn) {
+    return (
+      <div className="pet-profile">
+        <h2 className="title">Welcome {usernameInput}!</h2>
+        <button className="submit-btn" onClick={handleSignOut}>Sign Out</button>
+      </div>
+    );
+  }
 
   return (
-    <div className="Sign-UpForm">
-      {/* This is needed for the validation functionality above */}
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your signup!
-        </Alert>
+    <div className="pet-profile">
+      <h2 className="title">Signup/Login</h2>
+      <form className="form" onSubmit={handleSignupLogin}>
+        <label className="label">Email</label>
+        <input
+          className="input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='username'>Username</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-        </Form.Group>
+        <label className="label">Password</label>
+        <input
+          className="input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+        <label className="label">Username</label>
+        <input
+          className="input"
+          type="text"
+          placeholder="Username"
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+        />
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
+        <button className="submit-btn" type="submit">Sign up/Login</button>
+      </form>
     </div>
   );
 };
